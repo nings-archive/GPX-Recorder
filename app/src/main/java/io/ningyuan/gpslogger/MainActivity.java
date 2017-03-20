f4b50ea9fec87caad8381050a7686a980be543d8
ning <ningyuan.sg@gmail.com>
2017-03-20 11:05:41 +0800

    Add some toasts

--- app/src/main/java/io/ningyuan/gpslogger/MainActivity.java
+++ app/src/main/java/io/ningyuan/gpslogger/MainActivity.java
@@ -11,7 +11,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -21,6 +24,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     private TextView tv_latitude;
     private TextView tv_longitude;
+    private Button btn_record;
+    private Button btn_stop;
+    private Button btn_pause;
     private LocationManager locationManager;
     private Location location;
 
@@ -29,14 +35,27 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         setContentView(R.layout.activity_main);
         tv_latitude = (TextView) findViewById(R.id.lat_content);
         tv_longitude = (TextView) findViewById(R.id.lng_content);
+        btn_record = (Button) findViewById(R.id.btn_record);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
+        btn_pause = (Button) findViewById(R.id.btn_pause);
         locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
+
+        btn_pause.setEnabled(false);
+        btn_stop.setEnabled(false);
+        btn_record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, "I'M WORKING ON IT", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
 
         if (!GpsPermissions.isPermitted(this)) {
             GpsPermissions.ask(this);
         } else {
             initialiseLocation();
-            }
         }
+    }
 
 
     private void initialiseLocation () {
@@ -47,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 if (location != null) {
                     showLocation(location);
                 } else {
-                    // TODO(1): showGpsDisabledAlert()
+                    // ?
                 }
             }
         }
@@ -66,7 +85,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     initialiseLocation();
             } else {
-                    // alertdialog
+                Toast toast = Toast.makeText(MainActivity.this, "You must permit GPS permissions to use a GPS app!", Toast.LENGTH_LONG);
+                toast.show();
             }
         }
     }
@@ -78,6 +98,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override public void onStatusChanged(String provider, int status, Bundle extras) {}
     @Override public void onProviderEnabled(String provider) {}
-    @Override public void onProviderDisabled(String provider) {/*TODO(2): showGPSDisabled...();))*/}
+    @Override public void onProviderDisabled(String provider) {
+        Toast toast = Toast.makeText(MainActivity.this, "You must enable GPS to use a GPS app!", Toast.LENGTH_LONG);
+        toast.show();
+    }
 
 }
